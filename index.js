3f49f3d6bab30ddf427ea4276872ab84e72afeb9
Mert Kahyaoğlu <mertkahyaoglu93@gmail.com>
2015-02-12 21:20:52 +0200

    update deps.

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 var express    = require('express'),
     app        = express(),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    port = process.env.PORT || 8080;
 
-// configure app to use bodyParser() which will get the data from a POST
 app.use(require('express-method-override')());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
-
-var port = process.env.PORT || 8080;
-
-// register routes
 app.use('/', require('./lib/routes').router);
 
 // start server
